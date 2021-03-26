de4aa1cfee645163fdf4cfbea5b0d6558f1b0f1a
NUKEDO <nukedo.4444.orz@gmail.com>
2021-03-26 11:21:45 +0900

    Create 188_C.cpp

---
+++ at_coder/ABC/C/188_C.cpp
@@ -0,0 +1,28 @@
+// https://atcoder.jp/contests/abc189/tasks/abc188_c
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, nop, rate, left = INT_MIN, right = INT_MIN, saveL, saveR; // nop:number of people
+  scanf("%d\n", &n);
+  nop = 1 << n;
+  for(int i = 0; i < nop; i++) {
+    scanf("%d\n", &rate);
+    if(i < nop / 2) { //左の勝ち上がり
+      if(rate > left) {
+        left = rate;
+        saveL = i;
+      }
+    } else { //右の勝ち上がり
+      if(rate > right) {
+        right = rate;
+        saveR = i;
+      }
+    }
+  }
+  int ans = (left > right) ? saveR : saveL; //負けた方のiを出力
+  ans++; // iは0からなので1からにする
+  
+  printf("%d\n", ans);
+}
