e1aeb335b859a203db3ea4f2f8223acb3aacd866
NUKEDO <nukedo.4444.orz@gmail.com>
2021-04-30 11:27:37 +0900

    Create ITP1_5_C.cpp

---
+++ aizu_online_judge/Re/ITP1_5_C.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int h, w;
+  while(1) {
+    scanf("%d %d\n", &h, &w);
+    if(h == 0 && w == 0) break;
+    
+    for(int i = 0; i < h; i++) {
+      for(int j = 0; j < w; j++) {
+        if((i + j) % 2 == 0) printf("#");
+        else printf(".");
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+}
