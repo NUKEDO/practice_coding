68fc112838318a07466579de96c9f8186be3aa3b
NUKEDO <nukedo.4444.orz@gmail.com>
2021-05-25 17:45:13 +0900

    Create kukan_step5.cpp

---
+++ paiza/level_up_to_rankA/kukan_step5.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 100000
+
+int n, m, l, u, a;
+vector<int> num(MAX), add(MAX + 1); //いもす法
+
+void printVec(vector<int> v) {
+  for(int i = 0; i < n; i++) {
+    printf("%d\n", v.at(i));
+  }
+}
+
+int main() {
+  scanf("%d %d\n", &n, &m);
+  for(int i = 0; i < n; i++) {
+    scanf("%d", &num.at(i));
+  }
+  
+  for(int i = 0; i < m; i++) {
+    scanf("%d %d %d", &l, &u, &a);
+    l--;
+    u--;
+    add.at(l) += a;
+    add.at(u + 1) -= a;
+  }
+  
+  for(int i = 0; i < n; i++) {
+    num.at(i) += add.at(i);
+    add.at(i + 1) += add.at(i);
+  }
+  
+  printVec(num);
+}
